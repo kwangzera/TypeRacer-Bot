2e9d2027ca2b0626c54bb28fed35d6a99899819a
Togohogo1 <59260907+Togohogo1@users.noreply.github.com>
2020-08-21 23:59:00 -0400

    Big updates
    - Selenium not used (subject to change in future) for typing, but rather bs4
    - Made a bot class
    - Support for all gamemodes

--- src/typeracer_bot.py
+++ src/typeracer_bot.py
@@ -1,79 +1,59 @@
 import time
 from bs4 import BeautifulSoup
-from pynput.keyboard import Key, Controller
+from pynput.keyboard import Controller
 from selenium import webdriver
 
-keyboard = Controller()
 
-driver = webdriver.Chrome("driver/chromedriver.exe")
-driver.get("https://play.typeracer.com/")  # Open in new window
+class TyperacerBot:
+    def __init__(self, link):
+        self.driver = webdriver.Chrome("driver/chromedriver.exe")
+        self.keyboard = Controller()
+        self.link = link
+        self.text = ""
 
+    def start(self):
+        self.driver.get(self.link)
 
-def bs4_find_text(driver):
-    src = driver.page_source
-    soup = BeautifulSoup(src, "html.parser")
-    txt = soup.findAll("span")
-    print(3)
+    def find_text(self):
+        src = self.driver.page_source
+        soup = BeautifulSoup(src, "html.parser")
+        txt = soup.findAll("span")
 
-    words = []
+        for i in txt:
+            if "unselectable" in str(i):
+                self.text += i.text
 
-    for i in txt:
-        if "unselectable" in str(i):
-            words.append(i.text)
+        print(f"\"{self.text}\"")
 
-    return "".join(words[:-1]) + f"{words[-1]}"
+    def type_text(self, delay):
+        for i in self.text:
+            time.sleep(delay)
+            self.keyboard.press(i)
+            self.keyboard.release(i)
 
-
-def bs4_type_text(para, delay):
-    time.sleep(3)
-
-    for i in para:
-        time.sleep(delay)
-        keyboard.press(i)
-        keyboard.release(i)
-
-
-def sel_find_text(driver):
-    global elem
-
-    # Input box
-    # Rn is only selection
-    elem = driver.find_element_by_xpath("//td/input[@type='text']")
-    print("2")
-
-    # Actual text
-    sp = driver.find_elements_by_xpath("//span[@unselectable='on']")
-    print("3")
-
-    words = []
-
-    for i in sp:
-        words.append(i.text)
-
-    return "".join(words[:-1]) + f" {words[-1]}"
-
-
-def sel_type_text(para, delay):
-    time.sleep(3)
-
-    # For faster: .split(" ") for whole words
-    # Will improve in future
-    for i in para:
-        time.sleep(delay)
-        elem.send_keys(i)
+        self.text = ""
 
 
 def main():
-    # Waits until input and then clicks "Enter a typing race"
-    delay = float(input("Set your delay between keypresses: "))
-    elem = driver.find_element_by_link_text("Enter a typing race").click()
-    print(1)
+    custom = input("Custom link? Y/N: ")
+    
+    if custom.upper() == "Y":
+        link = input("Custom link: ")
+    else:
+        link = "https://play.typeracer.com"
+
+    bot = TyperacerBot(link)
+    bot.start()
 
     while True:
-        input("Press enter when the race starts: ")
-        para = bs4_find_text(driver)
-        bs4_type_text(para, delay)
+        try:
+            delay = float(input("Press enter when the race starts.: "))
+        except ValueError:
+            delay = 0.1
+
+        bot.find_text()
+        time.sleep(1)
+        bot.type_text(delay)
 
 
-# The chrome window sometimes autocloses due to some pipe error
 main()
