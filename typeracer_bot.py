0a7aa929b3d0880217112a2782bad35edf1d0158
George Zhang <geetransit@gmail.com>
2020-09-01 20:49:24 -0400

    Fix error (WebDriverException)

--- typeracer_bot.py
+++ typeracer_bot.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 from pynput.keyboard import Controller
 
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, WebDriverException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
