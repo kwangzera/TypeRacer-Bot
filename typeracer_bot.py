7c9410aa89ad84f7193bb52fb46e980452fa8e39
Togohogo1 <59260907+Togohogo1@users.noreply.github.com>
2020-09-03 22:17:43 -0400

    more formatting, folder renaming

--- typeracer_bot.py
+++ typeracer_bot.py
@@ -4,7 +4,7 @@ from contextlib import suppress
 from bs4 import BeautifulSoup
 from pynput.keyboard import Controller
 
-from selenium.common.exceptions import WebDriverException
+from selenium.common.exceptions import TimeoutException, WebDriverException
 from selenium.webdriver import Chrome, Firefox, Edge
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
@@ -13,10 +13,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 # dict[name: lambda -> WebDriver]
 DRIVER_FUNCS = dict(
-    chrome=lambda: Chrome(executable_path="driver/chromedriver"),
-    firefox=lambda: Firefox(executable_path="driver/firefoxdriver"),
-    edge=lambda: Edge(executable_path="driver/edgedriver"),
+    chrome=lambda: Chrome(executable_path="drivers/chromedriver"),
+    firefox=lambda: Firefox(executable_path="drivers/firefoxdriver"),
+    edge=lambda: Edge(executable_path="drivers/edgedriver"),
 )
+
 
 def find_text(driver):
     """Return typeracer text."""
@@ -31,6 +32,9 @@ def find_text(driver):
     for i in span:
         if "unselectable" in str(i):
             text += i.text
+
+    if text:
+        print("- valid text successfully found")
 
     return text
 
@@ -47,12 +51,17 @@ def press_text(keyboard, text, delay):
 def send_text(driver, text, delay):
     """Send `text` to text input box first working driver in `drivers` dict."""
 
-    # Click the inputbox when it can be clicked
-    elem = WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.CLASS_NAME, "txtInput")))
+    try:
+        # Click the inputbox when it can be clicked
+        wait = WebDriverWait(driver, 15)
+        elem = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "txtInput")))
+    except TimeoutException:
+        print("! inputbox not found within 15s")
+    else:
+        for i in text:
+            time.sleep(delay)
+            elem.send_keys(i)
 
-    for i in text:
-        time.sleep(delay)
-        elem.send_keys(i)
 
 def get_driver(preferred=None, drivers=None):
     """Return first working driver instance in `drivers` dict."""
@@ -67,8 +76,6 @@ def get_driver(preferred=None, drivers=None):
     for func in drivers.values():
         with suppress(WebDriverException):
             return func()
-
-    raise LookupError(f"Driver not found: {preferred=!r}")
 
 
 def main(link="https://play.typeracer.com", default=0.1, driver=None):
@@ -90,7 +97,7 @@ def main(link="https://play.typeracer.com", default=0.1, driver=None):
 
         text = find_text(driver)
         time.sleep(1)
-        press_text(keyboard, text, delay)
+        send_text(driver, text, delay)
 
 
 if __name__ == "__main__":
